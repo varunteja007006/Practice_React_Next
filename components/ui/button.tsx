61aed029885b732477ad21fe0087771afa0a5f5d
varun <varunteja007006@gmail.com>
2024-09-20 22:02:58 +0530

    update the buttons

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -21,6 +21,8 @@ const buttonVariants = cva(
         link: "text-primary underline-offset-4 hover:underline",
         success:
           "text-white bg-green-500 hover:bg-green-600 hover:dark:text-black dark:bg-green-600 dark:hover:bg-green-600/[0.8]",
+        danger:
+          "text-white bg-red-500 hover:bg-red-600 hover:dark:text-black dark:bg-red-600 dark:hover:bg-red-600/[0.8]",
       },
       size: {
         default: "h-10 px-4 py-2",
